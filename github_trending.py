5501e024bf6bd735da23567fb480aa0b315edbe2
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2018-02-04 21:34:19 +0300

    Update github_trending.py

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,37 @@
+import requests
+from datetime import date, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(top_size, age):
+    url = 'https://api.github.com/search/repositories?' \
+          'q=created:>{}&sort=stars&order=desc'.\
+          format(str(date.today() - timedelta(days=age)))
+    repos_list = requests.get(url).json()['items'][:top_size]
+    return repos_list
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_list(repo):
+    url = 'https://api.github.com/repos/{}/issues'.format(repo['full_name'])
+    issues_list = requests.get(url).json()
+    issues_url_list = [issue['html_url'] for issue in issues_list]
+    return issues_url_list
+
+
+def output_issues_info(repos_list):
+    for n, repo in enumerate(repos_list):
+        print('\n№{} repository: {}, owner: {}'.
+              format(n+1, repo['name'], repo['owner']['login']))
+        issues_url_list = get_open_issues_list(repo)
+        print('Open issues amount: {}'.format(len(issues_url_list)))
+        if len(issues_url_list):
+            for url in issues_url_list:
+               print(url)
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    age = 7
+    repos_list = get_trending_repositories(top_size, age)
+    print('Top-{} repositories for last {} days on {}'.
+          format(top_size, age, date.today()))
+    output_issues_info(repos_list)
