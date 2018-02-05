6374270240ce395f0f9228c019e16e40b676d638
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2018-02-05 16:55:22 +0300

    Update github_trending.py

--- github_trending.py
+++ github_trending.py
@@ -1,16 +1,16 @@
-requests
+import requests
 from datetime import date, timedelta
 
 
-def get_trending_repositories(top_size=20, age=7):
+def get_trending_repositories(top_size=20, days=7):
     url = 'https://api.github.com/search/repositories'
-    payload = {'q': 'created:>{}'.
-               format(str(date.today() - timedelta(days=age))),
+    due_date = date.today() - timedelta(days=days)
+    payload = {'q': 'created:>{}'.format(str(due_date)),
                'sort': 'star',
                'order': 'desc',
                'page': '1',
                'per_page': top_size
-              }
+               }
     repos_list = requests.get(url, params=payload).json()['items']
     return repos_list
 
@@ -22,19 +22,18 @@ def get_open_issues_list(repo):
     return issues_url_list
 
 
-def output_results(repos_list):
-    for n, repo in enumerate(repos_list):
-        print('\n№{} repository: {}, owner: {}'.
-              format(n+1, repo['name'], repo['owner']['login']))
-        issues_url_list = get_open_issues_list(repo)
-        print('Open issues amount: {}'.format(len(issues_url_list)))
-        if len(issues_url_list):
-            for url in issues_url_list:
-                print(url)
+def output_results(index, repo, issues_url_list):
+    print('\n№{} repository: {}, owner: {}'.
+          format(index + 1, repo['name'], repo['owner']['login']))
+    print('Open issues amount: {}'.format(len(issues_url_list)))
+    if len(issues_url_list):
+        for url in issues_url_list:
+            print(url)
 
 
 if __name__ == '__main__':
     repos_list = get_trending_repositories()
-    print('Top-20 repositories for last 7 days on {}'.
-          format( date.today()))
-    output_results(repos_list)
+    print('Top-20 repositories for last 7 days on {}'.format(date.today()))
+    for index, repo in enumerate(repos_list):
+        issues_url_list = get_open_issues_list(repo)
+        output_results(index, repo, issues_url_list)
